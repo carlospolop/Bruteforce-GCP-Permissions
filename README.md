d1a1c88e58248fa1f86125a80727efa324d8637d
Carlos Polop <carlospolop@gmail.com>
2024-01-24 00:15:34 +0100

    improvements

--- README.md
+++ README.md
@@ -1,25 +1,39 @@
 # bf_my_gcp_perms
 
-Find which permissions the service account has access to
+Find which permissions a GCP principals has access to (you need to have credentials for it).
 
 ```bash
 python3 bf_my_gcp_perms.py -h
-usage: bf_my_gcp_perms.py [-h] -p PROJECT (-c CREDENTIALS | -t TOKEN)
+usage: bf_my_gcp_perms.py [-h] -p PROJECT [-v] [-T THREADS] [-s SERVICES] [-S SIZE] (-c CREDENTIALS | -t TOKEN)
 
 Check the permissions of a service account
 
-optional arguments:
+options:
   -h, --help            show this help message and exit
   -p PROJECT, --project PROJECT
                         Name of the project to use
+  -v, --verbose         Print the found permissions as they are found
+  -T THREADS, --threads THREADS
+                        Number of threads to use, be careful with rate limits. Default is 3.
+  -s SERVICES, --services SERVICES
+                        Comma separated list of GCP service by its api names to check only (e.g. filtering top 10 services: -s
+                        iam.,compute.,storage.,container.,bigquery.,cloudfunctions.,pubsub.,sqladmin.,cloudkms.,secretmanager.). Default is all services.
+  -S SIZE, --size SIZE  Size of the chunks to divide all the services into. Default is 50.)
   -c CREDENTIALS, --credentials CREDENTIALS
                         Path to credentials.json
   -t TOKEN, --token TOKEN
                         Raw access token
+
 
 # Using json creds
 python3 bf_my_gcp_perms.py -c /tmp/credentials.json -p project-name-1232
 
 # Using raw token
 python3 bf_my_gcp_perms.py -t <token> -p project-name-1232
+
+# Using gcloud credentials
+python3 bf_my_gcp_perms.py -v -p project-name-1232 -t $(gcloud auth print-access-token)
+
+# Checking permissions only in the top10 services
+python3 bf_my_gcp_perms.py -v -p project-name-1232 -t $(gcloud auth print-access-token) -s "iam.,compute.,storage.,container.,bigquery.,cloudfunctions.,pubsub.,sqladmin.,cloudkms.,secretmanager."
 ```
--- bf_my_gcp_perms.py
+++ bf_my_gcp_perms.py
@@ -1,14 +1,15 @@
 import argparse
 import requests
-import json
 import tqdm
 import re
 from bs4 import BeautifulSoup
-
 from google.oauth2 import service_account
 import google.oauth2.credentials
 import googleapiclient.discovery
-from time import sleep
+import concurrent.futures
+from concurrent.futures import ThreadPoolExecutor
+from threading import Lock
+
 
 def download_gcp_permissions():
     """Get list with all permissions og GCP (copied and modified from https://github.com/iann0036/iam-dataset/blob/main/gcp_get_permissions.py)"""
@@ -20,19 +21,18 @@ def download_gcp_permissions():
     frame_page = requests.get(frame_page_url).text
     parsed_frame_page = BeautifulSoup(frame_page, features="lxml")
 
-    result = []
+    results = []
 
     for row in parsed_frame_page.find('tbody').find_all('tr'):
         permission = row.find_all('td')[0].get('id')
-        result.append(permission)
+        results.append(permission)
 
-    return result
+    return results
 
-def check_permissions(perms, service, project):
+def check_permissions(perms, service, project, verbose):
     """Test if the user has the indicated permissions"""
 
     # Wait 1 second to avoid hitting the rate limit
-    sleep(1)
 
     # Get the permissions for the current project
     req = service.projects().testIamPermissions(
@@ -43,19 +43,21 @@ def check_permissions(perms, service, project):
     have_perms = []
 
     try:
+        if "apikeys.keys.list" in perms or "apikeys.keys.undelete" in perms:
+            _=1
         returnedPermissions = req.execute()
         have_perms = returnedPermissions.get("permissions", [])
-    except googleapiclient.errors.HttpError as e:
+    except googleapiclient.errors.HttpError as e: # Example error: <HttpError 400 when requesting https://cloudresourcemanager.googleapis.com/v1/projects/digital-bonfire-410512:testIamPermissions?alt=json returned "Permission policyremediatormanager.remediatorServices.enable is not valid for this resource.". Details: "Permission policyremediatormanager.remediatorServices.enable is not valid for this resource.">
         for perm in perms:
-            if perm in str(e):
+            if " "+perm+" " in str(e): #Add spaces to avoid problems
                 perms.remove(perm)
-                return check_permissions(perms, service, project)
+                return check_permissions(perms, service, project, verbose)
         
     except Exception as e:
         print("Error:")
         print(e)
     
-    if have_perms:
+    if have_perms and verbose:
         print(f"Found: {have_perms}")
     
     return have_perms
@@ -67,16 +69,22 @@ def divide_chunks(l, n):
         yield l[i:i + n]
  
 
-
 def main():
     parser = argparse.ArgumentParser(description='Check the permissions of a service account')
     parser.add_argument('-p','--project', help='Name of the project to use', required=True)
+    parser.add_argument('-v','--verbose', help='Print the found permissions as they are found', action='store_true')
+    parser.add_argument('-T','--threads', help='Number of threads to use, be careful with rate limits. Default is 3.', default=3, type=int)
+    parser.add_argument('-s','--services', help='Comma separated list of GCP service by its api names to check only (e.g. filtering top 10 services: -s iam.,compute.,storage.,container.,bigquery.,cloudfunctions.,pubsub.,sqladmin.,cloudkms.,secretmanager.). Default is all services.', default='', type=str)
+    parser.add_argument('-S','--size', help='Size of the chunks to divide all the services into. Default is 50.)', default=50)
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument('-c','--credentials', help='Path to credentials.json')
     group.add_argument('-t','--token', help='Raw access token')
     args = vars(parser.parse_args())
 
     project = args['project']
+    verbose = args['verbose']
+    n_threads = int(args['threads'])
+    services_grep = [s.strip() for s in args['services'].split(',')] if args['services'] else []
     if args.get('token'):
         access_token = args['token']
         credentials = google.oauth2.credentials.Credentials(access_token.rstrip())
@@ -93,20 +101,37 @@ def main():
     if list_perms is None or len(list_perms) == 0:
         print("Couldn't download the permissions")
         return
+    list_perms.sort()
 
     print(f"Downloaded {len(list_perms)} GCP permissions")
+    
+    if len(services_grep)>0:
+        # Filter only inte interesting services
+        list_perms = [perm for perm in list_perms for grep_perm in services_grep if grep_perm.lower() in perm.lower()]
+        print(f"Filtered to {len(list_perms)} GCP permissions")
 
-    # Create the service account client
-    service = googleapiclient.discovery.build(
-        "cloudresourcemanager", "v1", credentials=credentials
-    )
-
-    divided_list_perms = list(divide_chunks(list_perms, 25))
-
+    # Check permissions
+    divided_list_perms = list(divide_chunks(list_perms, 20))
     have_perms = []
-    for subperms in tqdm.tqdm(divided_list_perms):
+    have_perms_lock = Lock()  # Lock for thread-safe operations on have_perms
+
+    def thread_function(subperms):
+        # Create the service account client
+        # Create 1 per thread to avoid errors!
+        service = googleapiclient.discovery.build(
+            "cloudresourcemanager", "v1", credentials=credentials
+        )
+
         # Test the permissions
-        have_perms += check_permissions(subperms, service, project)
+        perms = check_permissions(subperms, service, project, verbose)
+        with have_perms_lock:  # Ensure thread-safe update of have_perms
+            have_perms.extend(perms)
+
+    # Setup ThreadPoolExecutor
+    with ThreadPoolExecutor(max_workers=n_threads) as executor, tqdm.tqdm(total=len(divided_list_perms)) as progress:
+        futures = {executor.submit(thread_function, subperms): subperms for subperms in divided_list_perms}
+        for future in concurrent.futures.as_completed(futures):
+            progress.update(1)  # Update tqdm progress for each completed thread
 
     print("[+] Your Permissions: \n- " + '\n- '.join(have_perms))
     print("")
